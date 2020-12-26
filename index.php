c056707eaaaefa5acf20517ba69ad04d8b01da2e
PanggaJave <lupangojave@gmail.com>
2020-12-27 01:44:31 +0800

    changes v2

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>hello</title>
+</head>
+<body>
+<center><h1>Undermaintenance</h1></center>
+</body>
+</html>
+
+
 <?php
 header("Location: home");
-//echo $_GET['id'];
+
+//header("Location: /home");
